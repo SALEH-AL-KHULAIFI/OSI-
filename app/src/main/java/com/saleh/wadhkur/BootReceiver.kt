9d566a0ceaa32f957b085ea2f3ce972f4391c5fc
SALEH-AL-KHULAIFI <new.mtn.new@gmail.com>
2026-09-13 13:43:17 +0300

    Update BootReceiver.kt

--- app/src/main/java/com/saleh/wadhkur/BootReceiver.kt
+++ app/src/main/java/com/saleh/wadhkur/BootReceiver.kt
@@ -5,12 +5,25 @@ import android.content.Context
 import android.content.Intent
 
 class BootReceiver : BroadcastReceiver() {
-    override fun onReceive(context: Context, intent: Intent?) {
+
+    override fun onReceive(
+        context: Context,
+        intent: Intent?
+    ) {
+
         if (
             intent?.action == Intent.ACTION_BOOT_COMPLETED ||
             intent?.action == Intent.ACTION_MY_PACKAGE_REPLACED
         ) {
-            ReminderScheduler.schedule(context)
+
+            /*
+             * إعادة جدولة الأنواع الثلاثة:
+             *
+             * 1. الأذكار العامة حسب الفاصل المختار
+             * 2. أذكار الصباح الساعة 06:00
+             * 3. أذكار المساء الساعة 17:00
+             */
+            ReminderScheduler.scheduleAll(context)
         }
     }
 }
